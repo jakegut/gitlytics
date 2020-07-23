95dcc4805e83c40cba42028e5e1eb4e7463d18ff
Jake Gutierrez <gutierr8@southwestern.edu>
2020-07-23 12:55:01 -0500

    Fix update commits task

--- api/tasks.py
+++ api/tasks.py
@@ -32,7 +32,7 @@ def update_git_data():
             commit.additions = commit_data['stats']['additions']
             commit.deletions = commit_data['stats']['deletions']
         except KeyError as e:
-            print("KEY ERROR", e)
+            print("KEY ERROR: " + str(e))
             continue
 
     db.session.commit()
